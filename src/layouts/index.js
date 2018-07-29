b35edbb4396a416a58f3f58d7ea3227ab82e182d
anadutova <d.anasztazia@gmail.com>
2018-07-29 16:09:36 +0200

    first commit

---
+++ src/layouts/index.js
@@ -0,0 +1,88 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import Link from "gatsby-link";
+
+import Header from '../components/header'
+import './index.css'
+import 'normalize.css'
+
+const ListLink = props =>
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
+
+const Layout = ({ children, data }) => (
+  <div>
+    <Helmet
+      title={data.site.siteMetadata.title}
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+    <Header siteTitle={data.site.siteMetadata.title} />
+    <div
+      style={{
+        margin: '0 auto',
+        maxWidth: 960,
+        padding: '0px 1.0875rem 1.45rem',
+        paddingTop: 0,
+      }}
+    >
+      {children()}
+    </div>
+  </div>
+)
+
+Layout.propTypes = {
+  children: PropTypes.func,
+}
+
+export default ({ children }) =>
+  <div style={{position: `relative`, height: `100%`}}>
+    <header style={
+      { marginBottom: `1.5rem`,
+        padding: `1rem`,
+        backgroundColor: `darkred`,
+        color: `white`,
+        position: `fixed`,
+        top: 0,
+        width: `100%`
+       }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <h3 style={{ display: `inline` }}>Parkett Klub</h3>
+      </Link>
+      <nav style={{ position: `absolute`, bottom: 0, right: 0 }}> 
+      <ul style={{ listStyle: `none`, margin: 0, display: `flex`, justifyContent: `flex-end` }}>
+        <ListLink to="/events">Események</ListLink>
+        <ListLink to="/community/">Közösség</ListLink>
+        <ListLink to="/media/">Média</ListLink>
+        <ListLink to="/knowledge-base/">Tudásbázis</ListLink>
+      </ul>
+      </nav>
+    </header>
+    <main style={{ margin: `0 auto` }}>
+    {children()}
+    </main>
+    <footer style={{
+      height: `7rem`,
+      backgroundColor: `darkred`,
+      position: `fixed`,
+      width: `100%`,
+      bottom: 0
+    }}>
+    </footer>
+  </div>
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
---
+++ src/utils/typography.js
@@ -0,0 +1,6 @@
+import Typography from "typography";
+import fairyGateTheme from "typography-theme-github";
+
+const typography = new Typography(fairyGateTheme);
+
+export default typography;
