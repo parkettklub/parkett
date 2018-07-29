6f4ff33110fff41980fa4bc0c1e581bdce83a041
Kovács Gergő <kiskovi97@gmail.com>
2018-07-29 22:31:10 +0200

    Implement
    Implementation in the pages
    css refactor
    Need install:
    npm install react-addons-css-transition-group

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import EventParty from '../components/EventDetails/EventParty';
+import EventWorkshop from '../components/EventDetails/EventWorkshop';
 
 const SecondPage = () => (
   <div>
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <EventParty/>
+    <EventWorkshop/>
+    
   </div>
 )
 
