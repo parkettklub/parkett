17c86a49577281af1fb4bcc8522033560fd0e41c
anadutova <d.anasztazia@gmail.com>
2018-10-09 16:14:24 +0200

    Formatting

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,9 +3,10 @@ import styles from "./Card.module.css"
 
 const Card = (props) => {
   <article className={styles.card}>
-    {this.props.title && <header className={styles.header}>
-      {this.props.title}
-    </header>}
+    {this.props.title &&
+      <header className={styles.header}>
+        {this.props.title}
+      </header>}
     {this.props.children}
   </article>
 }
