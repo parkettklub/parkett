f55c016e8a649d01c17854375c80f6e1c42342c7
kiskovi97 <kiskovi97@gmail.com>
2018-11-24 10:43:20 +0100

    Card fix

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,14 +2,16 @@ import React from 'react'
 import styles from "./Card.module.css"
 
 const Card = (props) => {
-  const {title} = this.props;
-  <article className={styles.card}>
-    {title &&
-      <header className={styles.header}>
-        {title}
-      </header>}
-    {this.props.children}
-  </article>
+  const { title } = props;
+  return (
+    <article className={styles.card}>
+      {title &&
+        <header className={styles.header}>
+          {title}
+        </header>}
+      {props.children}
+    </article>
+  )
 }
 
 export default Card;
