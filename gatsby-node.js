5d945b04939d8c465c80ad7b68c174e9c62e7a99
anadutova <d.anasztazia@gmail.com>
2018-12-30 02:50:31 +0100

    URL polyfill

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,4 +4,11 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+import 'babel-polyfill'
+
+exports.onClientEntry = () => {
+    // Don't need to do anything here, but if you don't 
+    // export something, the import won't work.
+}
+
  // You can delete this file if you're not using it
