78aa19eed2dae6dcfcb844e5a116bb17a0bd0867
kiskovi97 <kiskovi97@gmail.com>
2019-02-07 14:28:15 +0100

    Dances on Knowledge-Base

--- src/components/Dances.jsx
+++ src/components/Dances.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { fetchAll } from './FetchFunctions';
 import styles from './Dances.module.css';
 import Folk from './dancing-figures/FolkDance.svg';
 import Kizomba from './dancing-figures/Kizomba.svg';
@@ -15,29 +16,22 @@ class Dances extends React.Component {
     constructor() {
         super();
         this.state = {
-            dances: [{
-                id: 1,
-                name: 'salsa',
-                type: 0,
-                content: 'A salsa egy világszerte elterjedt társastánc neve, mely az utcai latin táncokhoz sorolható. A név közvetlen jelentése fűszeres szósz, melyet eredetileg a kubai zenék elnevezéseként használtak, de elterjedten használják a tánc jelölésére. Sokféle fajtája létezik ma. Ilyen például a kubai, Puerto Rico-i, kolumbiai, de vannak vonalon táncolt változatai is, mint a Los Angeles-i és a New York-i stílus.',
-            }, {
-                id: 2,
-                type: 1,
-                name: 'magyar néptánc',
-                content: 'Hamarosan bővebben...',
-            }, {
-                id: 3,
-                type: 2,
-                name: "rock 'n' roll",
-                content: 'Majd egyszer...',
-            }, {
-                id: 4,
-                type: 3,
-                name: 'kizomba',
-                content: 'Dél-Afrikából, az egykori portugál gyarmat Angolából indult hódító útjára a kizomba, melyet sokan - tévesen - egyfajta afrikai tangóként határoznak meg. Tény, hogy a táncban tetten érhetők a tangó alaplépései, de a tangó színpadiasságától, jellegzetes romantikus drámájától igencsak messze áll a laza, dögös, bulis kizomba. A kizomba szó egyébként nem is jelent mást, mint ünnepet, kikapcsolódást, szórakozást. A szamba elődjének számító, hagyományos angolai szemba, a francia Antillákról származó zouk zene, valamint az argentin tangó lépései álltak össze a kizombában egzotikus eleggyé.',
-            }],
+            dances: [],
             selectedid: null,
         };
+    }
+
+    componentDidMount() {
+        this.fetchDances();
+    }
+
+    fetchDances = () => {
+        fetchAll('dances').then(
+            response => response.json(),
+        ).then((myJson) => {
+            this.setState({ dances: myJson });
+            console.log(myJson);
+        });
     }
 
     select = (id) => {
@@ -74,31 +68,31 @@ class Dances extends React.Component {
 
 function Dance({ dance, selected, click }) {
     const {
-        type, name, id, content,
+        dance_type, name, id, content,
     } = dance;
-    let color;
-    let icon;
-    switch (type) {
-        case 0: color = styles.red; icon = Salsa; break;
-        case 1: color = styles.green; icon = Folk; break;
-        case 2: color = styles.yellow; icon = Swing; break;
-        case 3: color = styles.purple; icon = Kizomba; break;
-        default: color = styles.green; icon = Folk;
+    let { color } = dance_type;
+    const { image } = dance_type;
+    if (selected) {
+        color += 'A0';
     }
     return (
         <Card>
             <div
-                className={`${styles.dance} ${color} ${selected ? styles.open : ''}`}
+                className={`${styles.dance} ${selected ? styles.open : ''}`}
+                style={
+                    {
+                        backgroundColor: color,
+                        ...animationStyle,
+                    }
+                }
                 onClick={() => click(id)}
-                style={animationStyle}
                 key="card"
                 role="button"
                 onKeyDown={() => { }}
                 tabIndex={0}
             >
-                <div><img src={icon} alt="" /></div>
-                <div>
-                    {' '}
+                <div><img src={image} alt="" /></div>
+                <div className={styles.title}>
                     <strong>
                         {name.charAt(0).toUpperCase() + name.slice(1)}
                     </strong>
