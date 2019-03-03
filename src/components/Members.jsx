adeaf59530409490cfc978151b27c92c7400a106
kiskovi97 <kiskovi97@gmail.com>
2019-03-03 11:25:00 +0100

    Revert "First Try"
    This reverts commit cdf81edf3bea80979f444538b8e7797cf60df0b6.

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -17,7 +17,7 @@ class Members extends React.Component {
         this.fetchMembers();
     }
 
-    async fetchMembers() {
+    fetchMembers = async function () {
         const myJson = await fetchAll('members');
         this.setState({ members: myJson });
     }
