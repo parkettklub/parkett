0104b22f3c22b1419f7ca1e9c127ff4b07f66be5
kiskovi97 <kiskovi97@gmail.com>
2019-03-30 10:41:50 +0100

    Loading Fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,20 +6,20 @@ import HomePage from '../components/HomePage';
 import './Animation.css';
 
 const IndexPage = () => (
-  <ReactCssTransitionGroup
-    transitionName="fade"
-    transitionAppearTimeout={1000}
-    transitionEnterTimeout={500}
-    transitionAppear
-    transitionLeaveTimeout={500}
-  >
+  <div>
     <Header />
-    <div
-      className={styles.main}
+    <ReactCssTransitionGroup
+      transitionName="fade"
+      transitionAppearTimeout={1000}
+      transitionEnterTimeout={500}
+      transitionAppear
+      transitionLeaveTimeout={500}
     >
-      <HomePage />
-    </div>
-  </ReactCssTransitionGroup>
+      <div className={styles.main}>
+        <HomePage />
+      </div>
+    </ReactCssTransitionGroup>
+  </div>
 );
 
 export default IndexPage;
