58de537dd4e7dc14f5522c9530493db4f64652ac
kiskovi97 <kiskovi97@gmail.com>
2019-04-04 17:41:21 +0200

    Backend Error Notification

--- src/components/FetchFunctions.js
+++ src/components/FetchFunctions.js
@@ -1,49 +1,74 @@
 import { getToken, isLoggedIn } from '../utils/login';
 
 export async function fetchAll(type) {
-    const response = await fetch(`https://parkett-klub.herokuapp.com/${type}`);
-    return response.json();
+    try {
+        const response = await fetch(`https://parkett-klub.herokuapp.com/${type}`);
+        return response.json();
+    } catch (error) {
+        alert('Probléma lépett fel az adatbázissal való kommunikációval');
+        return [];
+    }
 }
 
 export async function fetchMe() {
-    if (isLoggedIn()) {
-        const response = await fetch('https://parkett-klub.herokuapp.com/users/me?token=' + getToken());
-        return response.json();
+    try {
+        if (isLoggedIn()) {
+            const response = await fetch('https://parkett-klub.herokuapp.com/users/me?token=' + getToken());
+            return response.json();
+        }
+        return {};
+    } catch (error) {
+        alert('Probléma lépett fel az adatbázissal való kommunikációval');
+        return {};
     }
-    return { name: 'Példa Béla', role: 'főszerep', email: 'barb@ra.com' };
 }
 
 export async function fetchPost(type, element) {
-    const response = await fetch(`https://parkett-klub.herokuapp.com/${type}`, {
-        method: 'POST',
-        body: JSON.stringify(element),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    alert(`(${element.id ? element.id : ''}. ${type}) ${element.name ? element.name : ''} ${element.title ? element.title : ''} is Saved`);
-    return response;
+    try {
+        const response = await fetch(`https://parkett-klub.herokuapp.com/${type}`, {
+            method: 'POST',
+            body: JSON.stringify(element),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        alert(`(${element.id ? element.id : ''}. ${type}) ${element.name ? element.name : ''} ${element.title ? element.title : ''} is Saved`);
+        return response;
+    } catch (error) {
+        alert('Probléma lépett fel az adatbázissal való kommunikációval');
+        return {};
+    }
 }
 
 export async function fetchDelete(type, id) {
-    const response = await fetch(`https://parkett-klub.herokuapp.com/${type}/${id}`, {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    alert(`(${id}. ${type} is Deleted`);
-    return response;
+    try {
+        const response = await fetch(`https://parkett-klub.herokuapp.com/${type}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        alert(`(${id}. ${type} is Deleted`);
+        return response;
+    } catch (error) {
+        alert('Probléma lépett fel az adatbázissal való kommunikációval');
+        return {};
+    }
 }
 
 export async function fetchPut(type, element) {
-    const response = fetch(`https://parkett-klub.herokuapp.com/${type}/${element.id}`, {
-        method: 'PUT',
-        body: JSON.stringify(element),
-        headers: {
-            'Content-Type': 'application/json',
-        },
-    });
-    alert(`(${element.id ? element.id : ''}. ${type}) ${element.name ? element.name : ''} ${element.title ? element.title : ''} is Saved`);
-    return response;
+    try {
+        const response = fetch(`https://parkett-klub.herokuapp.com/${type}/${element.id}`, {
+            method: 'PUT',
+            body: JSON.stringify(element),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        alert(`(${element.id ? element.id : ''}. ${type}) ${element.name ? element.name : ''} ${element.title ? element.title : ''} is Saved`);
+        return response;
+    } catch (error) {
+        alert('Probléma lépett fel az adatbázissal való kommunikációval');
+        return {};
+    }
 }
