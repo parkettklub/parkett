2cedb0a44672d86a84b107b0fc8b4644257040ad
kiskovi97 <kiskovi97@gmail.com>
2019-05-01 12:33:53 +0200

    Icons

--- src/components/Header-Footer/LanguageSelector.jsx
+++ src/components/Header-Footer/LanguageSelector.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import styles from './Language.module.css';
 import { getLanguage, getLanguageName, setLanguage } from '../../utils/language';
 import strings from '../../utils/Descriptions';
+import enIcon from './english.svg';
+import huIcon from './hungary.svg';
 
 class LanguageSelector extends React.Component {
     constructor() {
@@ -26,7 +28,7 @@ class LanguageSelector extends React.Component {
                             onKeyDown={() => { }}
                             tabIndex={0}
                         >
-                            {'HU'}
+                            <img src={huIcon} alt="" />
                         </span>
                     </div>
                     <div className={`${styles.lang} ${styles.lang}`}>
@@ -36,7 +38,7 @@ class LanguageSelector extends React.Component {
                             onKeyDown={() => { }}
                             tabIndex={0}
                         >
-                            {'ENG'}
+                            <img src={enIcon} alt="" />
                         </span>
                     </div>
                 </div>
