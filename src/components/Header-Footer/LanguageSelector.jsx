e327dae524a669ade14efd9af45a747fa9dff045
kiskovi97 <kiskovi97@gmail.com>
2019-05-01 12:56:24 +0200

    refactor

--- src/components/Header-Footer/LanguageSelector.jsx
+++ src/components/Header-Footer/LanguageSelector.jsx
@@ -5,47 +5,37 @@ import strings from '../../utils/Descriptions';
 import enIcon from './english.svg';
 import huIcon from './hungary.svg';
 
-class LanguageSelector extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            language: null,
-        };
-    }
-
-    render() {
-        strings.setLanguage(getLanguage());
-        return (
-            <div>
-                <div className={styles.main}>
-                    <div className={styles.langName}>
-                        {getLanguageName()}
-                    </div>
-                    <div className={`${styles.lang} ${getLanguage() === 'hu' ? styles.selected : ''}`}>
-                        <span
-                            onClick={() => setLanguage('hu')}
-                            role="button"
-                            onKeyDown={() => { }}
-                            tabIndex={0}
-                        >
-                            <img src={huIcon} alt="" />
-                        </span>
-                    </div>
-                    <div className={`${styles.lang} ${getLanguage() === 'en' ? styles.selected : ''}`}>
-                        <span
-                            onClick={() => setLanguage('en')}
-                            role="button"
-                            onKeyDown={() => { }}
-                            tabIndex={0}
-                        >
-                            <img src={enIcon} alt="" />
-                        </span>
-                    </div>
+const LanguageSelector = () => {
+    strings.setLanguage(getLanguage());
+    return (
+        <div>
+            <div className={styles.main}>
+                <div className={styles.langName}>
+                    {getLanguageName()}
+                </div>
+                <div className={`${styles.lang} ${getLanguage() === 'hu' ? styles.selected : ''}`}>
+                    <span
+                        onClick={() => setLanguage('hu')}
+                        role="button"
+                        onKeyDown={() => { }}
+                        tabIndex={0}
+                    >
+                        <img src={huIcon} alt="" />
+                    </span>
+                </div>
+                <div className={`${styles.lang} ${getLanguage() === 'en' ? styles.selected : ''}`}>
+                    <span
+                        onClick={() => setLanguage('en')}
+                        role="button"
+                        onKeyDown={() => { }}
+                        tabIndex={0}
+                    >
+                        <img src={enIcon} alt="" />
+                    </span>
                 </div>
             </div>
-        );
-    }
-}
-
+        </div>
+    );
+};
 
 export default LanguageSelector;
